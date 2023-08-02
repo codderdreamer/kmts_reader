a44f4e437bdd4c4d3101a21ad4f70168dfdfddda
codderdreamer <sevdayucedagg@gmail.com>
2023-08-02 17:53:37 +0300

    dene

--- static/websocket.js
+++ static/websocket.js
@@ -52,12 +52,11 @@ function connect(){
             document.getElementById("seriNo1-result").className = ""
             document.getElementById("seriNo2-result").className = ""
             document.getElementById("seriNo3-result").className = ""
-            caches.keys().then(list => list.map(key => caches.delete(key)))
             
         } else if (incomingData.Command == "colourful-img-text") {
             document.getElementById("colourful-img-text").innerHTML = "Renkli görüntü alınıyor..."
         } else if (incomingData.Command == "colourful-img") {
-            document.getElementById("colourful-img").src = "../static/assets/test_1/1/colourful.png"
+            document.getElementById("colourful-img").src = "../static/assets/test_1/1/colourful.png?" + Math.random();
         } else if (incomingData.Command == "dataMatrix-text") {
             document.getElementById("dataMatrix-text").innerHTML = "Data matrix bulunuyor ..."
         } else if (incomingData.Command == "dataMatrix-img") {
