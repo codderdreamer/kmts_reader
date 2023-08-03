6f788577d75edc25bedb1a5399d057aff45808ec
codderdreamer <sevdayucedagg@gmail.com>
2023-08-03 15:08:52 +0300

    dene

--- Modules/CameraModule.py
+++ Modules/CameraModule.py
@@ -121,16 +121,16 @@ class CameraModule():
         x1 = 0
         y1 = 0
         x2 = 0
-        y2 = int(self.application.config.camera.colourful.resolution_seriNo_1)
-        x3 = int(self.application.config.camera.colourful.resolution_seriNo_1)
+        y2 = 70
+        x3 = 245
         y3 = 0
-        x4 = int(self.application.config.camera.colourful.resolution_seriNo_1)
-        y4 = int(self.application.config.camera.colourful.resolution_seriNo_1)
+        x4 = 245
+        y4 = 70
         return np.float32([[x1,y1],[x2,y2],[x3,y3],[x4,y4]])
     
     def cut_colourful_image_seriNo_1(self,frame):
         matrix = cv2.getPerspectiveTransform(self.get_colourful_seriNo_1_points(),self.get_colourful_seriNo_1_resolution_points())
-        frame = cv2.warpPerspective(frame,matrix,(int(self.application.config.camera.colourful.resolution_seriNo_1),int(self.application.config.camera.colourful.resolution_seriNo_1)))
+        frame = cv2.warpPerspective(frame,matrix,(245,70))
         return frame
     
 # Seri No 2 Cutting
